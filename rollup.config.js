f75ec54303683e7200fd637082c821d648b3cfdf
Tom Weatherhead <tomw@2hrd4u.org>
2021-12-01 12:32:00 -0700

    Dev

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,44 @@
 // rollup.config.js
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { terser } = require('rollup-plugin-terser');
+/**
+ * Copyright (c) Tom Weatherhead. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
 
-export default [
-	{
-		input: './dist/types/main.js',
-		output: [
-			{
-				file: 'dist/thaw-macd.cjs.js',
-				format: 'cjs',
-				exports: 'named'
-			},
-			{
-				file: 'dist/thaw-macd.esm.js',
-				format: 'es',
-				compact: true,
-				plugins: [terser()]
-			},
-			{
-				file: 'dist/thaw-macd.js',
-				name: 'thaw-macd',
-				format: 'umd',
-				compact: true,
-				plugins: [terser()]
-			}
-		]
-	}
-];
+'use strict';
+
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+	input: './dist/lib/main.js',
+	output: [
+		{
+			// Create a CommonJS version for Node.js
+			file: 'dist/thaw-macd.cjs.js',
+			format: 'cjs',
+			exports: 'named'
+		},
+		{
+			// Create an ESModule version
+			file: 'dist/thaw-macd.esm.js',
+			format: 'es',
+			esModule: true,
+			compact: true,
+			plugins: [terser()]
+		},
+		{
+			// Create a version that can run in Web browsers
+			file: 'dist/thaw-macd.js',
+			name: 'thaw-macd',
+			format: 'umd',
+			compact: true,
+			plugins: [terser()]
+		},
+	],
+	// context: 'window'
+	context: 'this',
+	plugins: [nodeResolve()]
+};
